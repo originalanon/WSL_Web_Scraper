fbc42af35933e09701e308c5aa7ebf847205b479
lbarton <lbart003@odu.edu>
2021-04-26 15:31:54 -0400

    Added functionality to pull data from second worksheet

--- WebScraper.py
+++ WebScraper.py
@@ -1,7 +1,9 @@
 from tableauscraper import TableauScraper as TS
 
 url = "https://cruisemarketwatch.com/market-share/"
+
 ts = TS()
 ts.loads(url)
-spreadSheet = ts.getWorkbook()
+
+spreadSheet = ts.getWorksheet("Details") #the tableau test-id of the second worksheet
 print(spreadSheet.data)
