7042d21dea9973bc77a8fdeece3858c23ef72bc8
lbarton <lbart003@odu.edu>
2021-04-26 17:46:17 -0400

    Added formatting to show all lines of the Tableau sheet

--- WebScraper.py
+++ WebScraper.py
@@ -1,5 +1,6 @@
 import sys as sys
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from tableauscraper import TableauScraper as TS
 
@@ -10,11 +11,13 @@ ts = TS()
 ts.loads(url)
 spreadSheet = ts.getWorksheet("Details")
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
 sys.stdout = open("SpreadsheetInfoFirst.txt", "w")
-for t in spreadSheet.worksheets:
-    print(t.data)
+print(spreadSheet.data)
 sys.stdout.close()
 
 
 
-soup = BeautifulSoup(page.content, 'html.parser')
+#soup = BeautifulSoup(page.content, 'html.parser')
