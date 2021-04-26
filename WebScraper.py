951a5c36c36378b34cae9d6eebec8d63ca8c6b02
originalanon <49574647+originalanon@users.noreply.github.com>
2021-04-26 17:12:33 -0400

    Updated WebScraper.py

--- WebScraper.py
+++ WebScraper.py
@@ -1,11 +1,14 @@
+import sys as sys
 from tableauscraper import TableauScraper as TS
 
 url = "https://cruisemarketwatch.com/market-share/"
 
 ts = TS()
 ts.loads(url)
-spreadSheet = ts.getWorkbook()
-print(spreadSheet.data)
+spreadSheet = ts.getWorksheet("Details")
 
-spreadSheet = ts.getWorksheet("Details") #the tableau test-id of the second worksheet
-print(spreadSheet.data)
+sys.stdout = open("SpreadsheetInfoFirst.txt", "w")
+for t in spreadSheet.worksheets:
+    print(t.data)
+sys.stdout.close()
+
