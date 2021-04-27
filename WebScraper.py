f816eddb546e9b2ac585181fd72befa70d82a574
Jake Mednikov <jmednikov13@gmail.com>
2021-04-27 10:54:00 -0700

    hello

--- WebScraper.py
+++ WebScraper.py
@@ -1,6 +1,8 @@
 import sys as sys
 import requests
 import pandas as pd
+import pandas_profiling
+import string as str
 from bs4 import BeautifulSoup
 from tableauscraper import TableauScraper as TS
 
@@ -18,6 +20,12 @@ pd.set_option('display.width', None)
 sys.stdout = open("SpreadsheetInfoFirst.csv", "w")
 print(spreadSheet.data)
 sys.stdout.close()
+
+#adding cleaning
+cruise_info=pd.read_csv("./SpreadsheetInfoFirst.csv", encoding= 'latin1')
+cruise_info=cruise_info.drop(["Brand-alias", "Parent-alias","Measure Names-value"],axis=1)
+cruise_info=cruise_info.drop_duplicates(inplace=True,subset="title")
+cruise_info.columns=["Parent Company", "Brand", "Total Passengers", "% of Passengers", "Revenue", "% of Revenue", "Average Stock Value"]
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
