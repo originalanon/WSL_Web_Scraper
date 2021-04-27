9979306892df08f96cd349563e68f766a4bfc9d9
lbarton <lbart003@odu.edu>
2021-04-27 17:37:35 -0400

    Reformatted SpreadsheetInfo to a TRUE CSV

--- WebScraper.py
+++ WebScraper.py
@@ -1,15 +1,14 @@
 import sys as sys
 import requests
 import pandas as pd
-import pandas_profiling
 import string as str
 from bs4 import BeautifulSoup
 from tableauscraper import TableauScraper as TS
-from csvFunctions import csvHeadersInsert as csvHeaders
+import csvFunctions as cf
 
-CSV_EXPORT_FILE = "CruiseData.csv"
 url = "https://cruisemarketwatch.com/market-share/"
 
+CSV_EXPORT_FILE = "./CruiseData.csv"
 csvHeaders(CSV_EXPORT_FILE)
 
 ts = TS()
@@ -20,15 +19,9 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 
-sys.stdout = open("SpreadsheetInfoFirst.csv", "w")
-print(spreadSheet.data)
-sys.stdout.close()
+cf.organizeSpreadsheetInfo("./SpreadsheetInfoFirst.csv")
 
-#adding cleaning
-cruise_info=pd.read_csv("./SpreadsheetInfoFirst.csv", encoding= 'latin1')
-cruise_info=cruise_info.drop(["Brand-alias", "Parent-alias","Measure Names-value"],axis=1)
-cruise_info=cruise_info.drop_duplicates(inplace=True,subset="title")
-cruise_info.columns=["Parent Company", "Brand", "Total Passengers", "% of Passengers", "Revenue", "% of Revenue", "Average Stock Value"]
+print(spreadSheet.data.to_csv("./SpreadSheetInfoFirst.csv"))
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
--- csvFunctions.py
+++ csvFunctions.py
@@ -1,7 +1,21 @@
 import sys as sys
+import re
 
 def csvHeadersInsert(file):
     sys.stdout = open(file, "w")
     print("Parent Company, Brand, Total Passengers, % of Passengers, Revenue, % of Revenue, Average Stock Value")
     sys.stdout.close()
 
+def organizeSpreadsheetInfo(file):
+    csvFile = open(file, "r")
+    while True:
+        line = csvFile.readline()
+        if not line:
+            break
+
+        line = line.replace(',', '\n', 7)
+
+        print(line)
+        
+
+    csvFile.close()
