9a5c0f0cada1ef51dbed9fded93be8195657a73b
originalanon <49574647+originalanon@users.noreply.github.com>
2021-04-28 15:10:08 -0400

    Update CruiseStats.py

--- CruiseStats.py
+++ CruiseStats.py
@@ -1,27 +1,30 @@
 class CruiseStats():
-        def __init__(self, name, parentCompany,totalPassengers, percentOfPassengers, revenue, percentOfRevenue ):
-        
-                self.name=name
-                self.parentCompany=parentCompany
-                self.totalPassenger=totalPassengers
-                self.percentOfPassengers=percentOfPassengers
-                self.revenue=revenue
-                self.percentOfRevenue=percentOfRevenue
 
-        def setName(self,x):
-                self.name=x
-        def setParentCompany(self,x):
-                self.parentCompany=x
-        def setTotalPassengers(self,x):
-                self.totalPassengers=x
-        def setPercentOfPassengers(self,x):
-                self.percentOfPassengers=x
-        def setRevenue(self,x):
-            self.revenue=x    
-        def setPercentOfRevenue(self,x):
-                self.percentOfRevenue=x
+    def __init_(self, name = "", parentCompany = "" ,totalPassengers = "", percentOfPassengers = "", revenue = "", percentOfRevenue = "" ):
+        self.name=name
+        self.parentCompany=parentCompany
+        self.totalPassengers=totalPassengers
+        self.percentOfPassengers=percentOfPassengers
+        self.revenue=revenue
+        self.percentOfRevenue=percentOfRevenue
 
-        def printStatistic(self):
-                print(self.name + ', ' + self.parentCompany +', ' + self.totalPassengers+', '
-                +self.setPercentOfPassengers +', '+self.revenue+ ', ' +self.percentOfRevenue)
+    def setName(self,x):
+        self.name=x
+    def setParentCompany(self,x):
+        self.parentCompany=x
+    def setTotalPassengers(self,x):
+        self.totalPassengers=x
+    def setPercentOfPassengers(self,x):
+        self.percentOfPassengers=x
+    def setRevenue(self,x):
+        self.revenue=x    
+    def setPercentOfRevenue(self,x):
+        self.percentOfRevenue=x
+
+    def getPassengers(self):
+        return self.totalPassengers
+
+    def printStatistic(self):
+        print(str(self.name + ', ' + self.parentCompany +', '
+        +self.setPercentOfPassengers +', '+self.revenue+ ', ' +self.percentOfRevenue))
                 
